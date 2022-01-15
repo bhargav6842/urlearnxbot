94fa8538ebbf8080ff029b47ba0caeaa1e0f691b
bhargav6842 <61858895+bhargav6842@users.noreply.github.com>
2022-01-15 14:24:13 +0530

    done
    done

---
+++ README.md
@@ -0,0 +1,30 @@
+# Droplink-bot
+Make short link by using [Droplink](https://droplink.co/ref/DAKSHYADAV) API key. Made by @dakshy. Give a star 🌟 and fork 🍴 it.
+
+### New Features!!
+
+```
++ Shorten Many Links in a message. [For More Info, Check Screenshots.]
+```
+
+### Installation
+
+<p><a href="https://heroku.com/deploy?template=https://github.com/dakshy/droplink-bot"> <img src="https://img.shields.io/badge/Deploy%20To%20Heroku-blueviolet?style=for-the-badge&logo=heroku" width="200"/></a><br/>
+<a href="https://railway.app/new/template?template=https%3A%2F%2Fgithub.com%2Fdakshy%2Fdroplink-bot&envs=API_KEY%2CAPI_HASH%2CAPI_ID%2CBOT_TOKEN"><img src="https://img.shields.io/badge/Deploy%20To%20Railway-purple?style=for-the-badge&logo=railway" width="200"/></a></p>
+
+##### Required Variables
+
+* `BOT_TOKEN`: Create a bot using [@BotFather](https://t.me/BotFather), and get the Telegram API token.
+* `API_ID`: Get this value from [my.telegram.org](https://my.telegram.org/apps).
+* `API_HASH`: Get this value from [my.telegram.org](https://my.telegram.org/apps).
+* `API_KEY`: Create [Droplink](https://droplink.co/ref/DAKSHYADAV) account then get this value from [Droplink API](https://droplink.co/ref/DAKSHYADAV).
+
+### How to Use?
+
+* Star and Fork this repo.
+* Deploy the bot.
+* Start your bot.
+* Send any link to the bot.
+* Bot will autodetect and send you the shortened url.
+
+<a href="https://dakshy.github.io/droplink-bot/screenshots/"><img src="https://img.shields.io/badge/See%20Screenshots-white?style=for-the-badge&logo=telegram" width="200"/></a>
---
+++ bot.py
@@ -0,0 +1,63 @@
+# DO NOT REMOVE CREDITS
+# Copyright (c) 2021 dakshy/droplink-bot
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from os import environ
+import aiohttp
+from pyrogram import Client, filters
+
+API_ID = environ.get('API_ID')
+API_HASH = environ.get('API_HASH')
+BOT_TOKEN = environ.get('BOT_TOKEN')
+API_KEY = environ.get('API_KEY', 'e3eddb3e7c5513eee187120fce788ddc4a1a643b')
+
+bot = Client('droplink bot',
+             api_id=API_ID,
+             api_hash=API_HASH,
+             bot_token=BOT_TOKEN,
+             workers=50,
+             sleep_threshold=10)
+
+
+@bot.on_message(filters.command('start') & filters.private)
+async def start(bot, message):
+    await message.reply(
+        f"**Hi {message.chat.first_name}!**\n\n"
+        "I'm a specialised bot for shortening links which can help you earn money by just sharing links. Made by <a href=\"https://github.com/dakshy\">ToonsHub</a>.")
+
+
+@bot.on_message(filters.regex(r'https?://[^\s]+') & filters.private)
+async def link_handler(bot, message):
+    links = message.text
+    links = links.split("\n")
+    for num in range(len(links)):
+      try:
+        short_link = await get_shortlink(links[num])
+        await message.reply(f'**Long URL:** {links[num]}\n**Shortened URL:** {short_link}\n\nMade by bhuro', quote=True, disable_web_page_preview=True)
+      except Exception as e:
+        await message.reply(f'Error: {e}', quote=True)
+
+
+async def get_shortlink(link):
+    url = 'https://urlearnx.com/api'
+    params = {'api': API_KEY, 'url': link}
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params, raise_for_status=True) as response:
+            data = await response.json()
+            return data["shortenedUrl"]
+
+
+bot.run()
